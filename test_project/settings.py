5dfb6f0652a201504a991bc11b5346d4dcc03170
Muhammad Ayaz <166490310+ayazkhattakup@users.noreply.github.com>
2024-06-04 00:56:39 +0500

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -49,8 +49,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'NoorDB',
+        'USER': 'ayaz',
+        'PASSWORD': 'NooralQuran@8000localhost',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
